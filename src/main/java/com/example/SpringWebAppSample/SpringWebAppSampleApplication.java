4e45bf5174235bc85cdde20b36577e3c932470b5
nidhinlalcv <nidhinlalcv@gmail.com>
2020-08-04 12:35:06 +0530

    project created

---
+++ src/main/java/com/example/SpringWebAppSample/SpringWebAppSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebAppSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringWebAppSample/SpringWebAppSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebAppSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
